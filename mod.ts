f3bc69aa78454bb9484b92d3b2518081093ad5a8
jabonkun <134466249+jabonkun@users.noreply.github.com>
2023-05-28 21:44:56 -0400

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -1,1 +1,18 @@
+// Load and apply the patches/handlers
+import * as winSighup from './lib/win-sighup.ts'
+import * as signals from './lib/wrappers/signals.ts'
+import * as exit from './lib/wrappers/exit.ts'
+
+// Make sure TSC don't thinks those imports are not used
+try {
+    // @ts-ignore
+    winSighup();
+    // @ts-ignore
+    signals();
+    // @ts-ignore
+    exit();
+} catch {
+    // Well, you're calling non-callable objects, what did u expected?
+}
+
 export { default } from './lib/recyclable.ts'
